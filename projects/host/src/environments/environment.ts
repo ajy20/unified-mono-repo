c3bbc9cf2c3355be926fc7a8c96f0bb7ce307814
Ajay Pawar <jpawaraj@jci.com>
2026-01-13 20:47:30 +0530

    Updated changes

--- projects/host/src/environments/environment.ts
+++ projects/host/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   microFrontends: {
     // Replace with your actual Azure URL
-    products: 'https://happy-field-02889950f.2.azurestaticapps.net/remoteEntry.json' 
+    products: '/products/remoteEntry.json'
   },
   federationManifestFileName: 'federation.manifest.json'
 };
