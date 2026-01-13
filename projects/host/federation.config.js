3ffd86a54eac9568ab66cb4c70eb37306896cdeb
Ajay Pawar <jpawaraj@jci.com>
2026-01-13 20:27:51 +0530

    Updated build script

--- projects/host/federation.config.js
+++ projects/host/federation.config.js
@@ -1,7 +1,7 @@
 const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
-
+  name: 'host',
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
   },
