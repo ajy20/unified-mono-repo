578800600a6eee62073f4effa4622fd4b9a7efb4
Ajay Pawar <jpawaraj@jci.com>
2026-01-14 12:18:07 +0530

    Remote app updated

--- projects/host/src/environments/environment.ts
+++ projects/host/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   microFrontends: {
-    // Replace with your actual Azure URL
     products: '/products/remoteEntry.json'
   },
   federationManifestFileName: 'federation.manifest.json'
